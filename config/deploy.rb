381a60baf30fc729b5f44327bb2bcf08709e6f0e
root <watermelontech>
2020-10-31 15:50:23 +0530

    Changes in deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 # Change these
-# server '165.22.119.35', port: 22, roles: [:web, :app, :db], primary: true
-server '165.22.119.35', user: 'gokul', roles: %w{app db web}
-
+server '165.22.119.35', port: 22, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:gstarle92/you-tube-clone.git'
 set :application,     'you-toube-clone'
@@ -20,7 +18,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
